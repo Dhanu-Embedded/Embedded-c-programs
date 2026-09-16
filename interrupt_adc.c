e1a4fb65964c46a602badd78395bd0861c9926da
Dhanu-Embedded <sreedhanu306@gmail.com>
2026-09-16 21:52:06 +0530

    Interrupt based ADC control

---
+++ interrupt_adc.c
@@ -0,0 +1,64 @@
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+
+#define LED RB0
+
+unsigned int adc;
+
+void main(void)
+{
+    TRISB = 0x00;
+
+    ADCON0 = 0B00000001;
+    ADCON1 = 0B10000000;
+
+    LED = 0;
+
+    ADIF = 0;
+    ADIE = 1;
+    PEIE = 1;
+    GIE = 1;
+
+    while(1)
+    {
+        GO_nDONE = 1;
+        __delay_ms(10);
+    }
+
+    return;
+}
+
+void __interrupt() timer(void)
+{
+    if(ADIF == 1)
+    {
+        adc = (ADRESH << 8) | ADRESL;
+
+        if(adc > 1000)
+        {
+            LED = 1;
+        }
+        else
+        {
+            LED = 0;
+        }
+
+        ADIF = 0;
+    }
+}
