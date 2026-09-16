e00a925cf231a95f867c4528427a428e4d142b65
Dhanu-Embedded <sreedhanu306@gmail.com>
2026-09-16 21:39:38 +0530

    Add LCD display program

---
+++ hello.c
@@ -0,0 +1,57 @@
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = ON         // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+#define RS RB0
+#define RW RB1
+#define EN RB2
+void lcd_cmd(unsigned char cmd)
+{
+    PORTD = cmd;
+    RS = 0;
+    RW = 0;
+    EN = 1;
+    __delay_ms(2);
+    EN = 0;
+}
+void lcd_data(unsigned char data)
+{
+    PORTD = data;
+    RS = 1;
+    RW = 0;
+    EN = 1;
+    __delay_ms(2);
+    EN = 0;
+}
+void main(void)
+{
+    TRISB = 0x00;
+    TRISD = 0x00;
+    lcd_cmd(0x38);
+    lcd_cmd(0x0C);
+    lcd_cmd(0x01);
+    lcd_cmd(0x06);
+    lcd_cmd(0x80);
+    lcd_data('H');
+    lcd_data('E');
+    lcd_data('L');
+    lcd_data('L');
+    lcd_data('O');
+
+    while(1);
+}
